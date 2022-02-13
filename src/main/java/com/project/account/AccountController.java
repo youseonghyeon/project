7d7d0b5b50f95dbf82cf6c3504232357b9a6ff97
you <dolla_@naver.com>
2022-02-13 23:45:43 +0900

    회원가입 기능 진행중, 이메일 기능 추가

--- src/main/java/com/project/account/AccountController.java
+++ src/main/java/com/project/account/AccountController.java
@@ -1,14 +1,34 @@
 package com.project.account;
 
+import com.project.domain.Account;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder binder) {
+        binder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
@@ -17,12 +37,31 @@ public class AccountController {
     }
 
     @PostMapping("/sign-up")
-    public String signUp(@RequestBody @Validated SignUpForm signUpForm) {
-        if (signUpForm.getEmail().equals("dolla_@naver.com")) {
-            throw new IllegalStateException("중복 이메일");
+//    @Transactional
+    public String signUp(@ModelAttribute @Validated SignUpForm signUpForm, Errors errors) {
+        // Attribute로 SignUpForm이 들어오므로 InitBinder 가 실행된다.
+        if (errors.hasErrors()) {
+            return "account/sign-up";
         }
-        System.out.println("signUpForm.getEmail() = " + signUpForm.getEmail());
-        System.out.println("signUpForm.getNickname() = " + signUpForm.getNickname());
-        return "/";
+
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) //TODO encoding 해야함
+                .studyCreatedByWeb(true)
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
+        Account newAccount = accountRepository.save(account);
+
+        newAccount.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("프로젝트, 회원 가입 인증");
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());
+        javaMailSender.send(mailMessage);
+
+        log.info("회원 가입이 완료되었습니다.");
+        return "redirect:/";
     }
 }
--- src/main/java/com/project/account/SignUpForm.java
+++ src/main/java/com/project/account/SignUpForm.java
@@ -2,23 +2,24 @@ package com.project.account;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
-import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class SignUpForm {
 
-    @Length(min = 4, max = 10)
     @NotEmpty
+    @Length(min = 3, max = 20)
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
     private String nickname;
 
     @Email
     @NotEmpty
     private String email;
 
-    @Length(min = 4, max = 10)
     @NotEmpty
+    @Length(min = 8, max = 50)
     private String password;
 }
