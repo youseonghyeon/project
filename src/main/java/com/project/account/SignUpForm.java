6663a11e4c537653b449f904f70b308c51291faf
you <dolla_@naver.com>
2022-02-13 18:12:13 +0900

    resources 추가

---
+++ src/main/java/com/project/account/SignUpForm.java
@@ -0,0 +1,13 @@
+package com.project.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+}
