e92b44997105620a545a6a89035103503037fb5c
you <dolla_@naver.com>
2022-02-13 18:40:57 +0900

    success

--- src/main/java/com/project/account/AccountController.java
+++ src/main/java/com/project/account/AccountController.java
@@ -2,7 +2,10 @@ package com.project.account;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class AccountController {
@@ -11,5 +14,15 @@ public class AccountController {
     public String signUpForm(Model model) {
         model.addAttribute(new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUp(@RequestBody @Validated SignUpForm signUpForm) {
+        if (signUpForm.getEmail().equals("dolla_@naver.com")) {
+            throw new IllegalStateException("중복 이메일");
+        }
+        System.out.println("signUpForm.getEmail() = " + signUpForm.getEmail());
+        System.out.println("signUpForm.getNickname() = " + signUpForm.getNickname());
+        return "/";
     }
 }
--- src/main/java/com/project/account/SignUpForm.java
+++ src/main/java/com/project/account/SignUpForm.java
@@ -1,13 +1,24 @@
 package com.project.account;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 public class SignUpForm {
 
+    @Length(min = 4, max = 10)
+    @NotEmpty
     private String nickname;
 
+    @Email
+    @NotEmpty
     private String email;
 
+    @Length(min = 4, max = 10)
+    @NotEmpty
     private String password;
 }
