1cf7c4424fb6c6bb83cc8d0a37a7565965cf8ae9
you <dolla_@naver.com>
2022-02-15 02:06:46 +0900

    account 수정 중

---
+++ src/main/java/com/project/main/MainController.java
@@ -0,0 +1,18 @@
+package com.project.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(@CurrentUser Account account, Model model) {
+        if (account != null) {
+            model.addAttribute(account);
+        }
+
+        return "index";
+    }
+}
